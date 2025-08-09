3841b1a8a2eaae7e38c0901ca5f94101ae76680d
kencreations <samontejovenie06@gmail.com>
2025-08-09 12:40:54 +0800

    .

---
+++ footer2.php
@@ -0,0 +1,117 @@
+<section class="call-to-action text-white text-center p-3" id="signup" style="background-color: #f4a261;"
+    bis_label="style">
+    <div class="container position-relative">
+        <div class="row justify-content-center">
+            <div class="col-xl-6">
+                <h3 class="mb-4 text-white">
+                    Ready to get started? Download Memo</h3>
+                <form class="form-subscribe" id="contactFormFooter" data-sb-form-api-token="API_TOKEN">
+
+                    <div class="row">
+                        <div class="col">
+                            <input class="form-control form-control-lg" id="emailAddressBelow" type="email"
+                                placeholder="Email Address" data-sb-validations="required,email"
+                                data-sb-can-submit="no">
+                            <div class="invalid-feedback text-white" data-sb-feedback="emailAddressBelow:required">
+                                Email Address is required.</div>
+                            <div class=" invalid-feedback text-white" data-sb-feedback="emailAddressBelow:email">
+                                Email Address Email is not valid.</div>
+                        </div>
+                        <div class=" col-auto"
+                            bis_size="{&quot;x&quot;:1154,&quot;y&quot;:3367,&quot;w&quot;:127,&quot;h&quot;:64,&quot;abs_x&quot;:1154,&quot;abs_y&quot;:3430}">
+                            <button class="btn btn-primary btn-lg disabled" id="submitButton" type="submit"
+                                bis_size="{&quot;x&quot;:1166,&quot;y&quot;:3367,&quot;w&quot;:103,&quot;h&quot;:64,&quot;abs_x&quot;:1166,&quot;abs_y&quot;:3430}">Submit</button>
+                        </div>
+                    </div>
+                    <div class="d-none" id="submitSuccessMessage">
+                        <div class=" text-center mb-3">
+                            <div class=" fw-bolder">
+                                Form submission successful!</div>
+                            <p>
+                                To activate this form, sign up at</p>
+                            <a class="text-white" href="https://startbootstrap.com/solution/contact-forms" ">https://startbootstrap.com/solution/contact-forms</a>
+                        </div>
+                    </div>
+                    <div class=" d-none" id="submitErrorMessage">
+                                <div class=" text-center text-danger mb-3">
+                                    Error sending message!
+                                </div>
+                        </div>
+
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
+<footer class="footer m-4">
+    <div class="container">
+        <div class=" row">
+            <div class=" col-lg-6 h-100 text-center text-lg-start my-auto">
+                <ul class="list-inline mb-2"
+                    bis_size="{&quot;x&quot;:304,&quot;y&quot;:3607,&quot;w&quot;:636,&quot;h&quot;:24,&quot;abs_x&quot;:304,&quot;abs_y&quot;:3670}">
+                    <li class="list-inline-item"
+                        bis_size="{&quot;x&quot;:304,&quot;y&quot;:3607,&quot;w&quot;:43,&quot;h&quot;:24,&quot;abs_x&quot;:304,&quot;abs_y&quot;:3670}">
+                        <a href="#!"
+                            bis_size="{&quot;x&quot;:304,&quot;y&quot;:3609,&quot;w&quot;:43,&quot;h&quot;:19,&quot;abs_x&quot;:304,&quot;abs_y&quot;:3672}">About</a>
+                    </li>
+                    <li class="list-inline-item"
+                        bis_size="{&quot;x&quot;:359,&quot;y&quot;:3607,&quot;w&quot;:5,&quot;h&quot;:24,&quot;abs_x&quot;:359,&quot;abs_y&quot;:3670}">
+                        ⋅</li>
+                    <li class="list-inline-item"
+                        bis_size="{&quot;x&quot;:375,&quot;y&quot;:3607,&quot;w&quot;:56,&quot;h&quot;:24,&quot;abs_x&quot;:375,&quot;abs_y&quot;:3670}">
+                        <a href="#!"
+                            bis_size="{&quot;x&quot;:375,&quot;y&quot;:3609,&quot;w&quot;:56,&quot;h&quot;:19,&quot;abs_x&quot;:375,&quot;abs_y&quot;:3672}">Contact</a>
+                    </li>
+                    <li class="list-inline-item"
+                        bis_size="{&quot;x&quot;:443,&quot;y&quot;:3607,&quot;w&quot;:5,&quot;h&quot;:24,&quot;abs_x&quot;:443,&quot;abs_y&quot;:3670}">
+                        ⋅</li>
+                    <li class="list-inline-item"
+                        bis_size="{&quot;x&quot;:459,&quot;y&quot;:3607,&quot;w&quot;:90,&quot;h&quot;:24,&quot;abs_x&quot;:459,&quot;abs_y&quot;:3670}">
+                        <a href="#!"
+                            bis_size="{&quot;x&quot;:459,&quot;y&quot;:3609,&quot;w&quot;:90,&quot;h&quot;:19,&quot;abs_x&quot;:459,&quot;abs_y&quot;:3672}">Terms
+                            of Use</a>
+                    </li>
+                    <li class="list-inline-item"
+                        bis_size="{&quot;x&quot;:560,&quot;y&quot;:3607,&quot;w&quot;:5,&quot;h&quot;:24,&quot;abs_x&quot;:560,&quot;abs_y&quot;:3670}">
+                        ⋅</li>
+                    <li class="list-inline-item"
+                        bis_size="{&quot;x&quot;:577,&quot;y&quot;:3607,&quot;w&quot;:97,&quot;h&quot;:24,&quot;abs_x&quot;:577,&quot;abs_y&quot;:3670}">
+                        <a href="#!"
+                            bis_size="{&quot;x&quot;:577,&quot;y&quot;:3609,&quot;w&quot;:97,&quot;h&quot;:19,&quot;abs_x&quot;:577,&quot;abs_y&quot;:3672}">Privacy
+                            Policy</a>
+                    </li>
+                </ul>
+                <p class="text-muted small mb-4 mb-lg-0"
+                    bis_size="{&quot;x&quot;:304,&quot;y&quot;:3639,&quot;w&quot;:636,&quot;h&quot;:21,&quot;abs_x&quot;:304,&quot;abs_y&quot;:3702}">
+                    © Your Website 2023. All Rights Reserved.</p>
+            </div>
+            <div class="col-lg-6 h-100 text-center text-lg-end my-auto"
+                bis_size="{&quot;x&quot;:952,&quot;y&quot;:3613,&quot;w&quot;:660,&quot;h&quot;:42,&quot;abs_x&quot;:952,&quot;abs_y&quot;:3676}">
+                <ul class="list-inline mb-0"
+                    bis_size="{&quot;x&quot;:964,&quot;y&quot;:3613,&quot;w&quot;:636,&quot;h&quot;:42,&quot;abs_x&quot;:964,&quot;abs_y&quot;:3676}">
+                    <li class="list-inline-item me-4"
+                        bis_size="{&quot;x&quot;:1462,&quot;y&quot;:3613,&quot;w&quot;:28,&quot;h&quot;:42,&quot;abs_x&quot;:1462,&quot;abs_y&quot;:3676}">
+                        <a href="#!"
+                            bis_size="{&quot;x&quot;:1462,&quot;y&quot;:3629,&quot;w&quot;:28,&quot;h&quot;:19,&quot;abs_x&quot;:1462,&quot;abs_y&quot;:3692}"><i
+                                class="bi-facebook fs-3"
+                                bis_size="{&quot;x&quot;:1462,&quot;y&quot;:3617,&quot;w&quot;:28,&quot;h&quot;:34,&quot;abs_x&quot;:1462,&quot;abs_y&quot;:3680}"></i></a>
+                    </li>
+                    <li class="list-inline-item me-4"
+                        bis_size="{&quot;x&quot;:1517,&quot;y&quot;:3613,&quot;w&quot;:28,&quot;h&quot;:42,&quot;abs_x&quot;:1517,&quot;abs_y&quot;:3676}">
+                        <a href="#!"
+                            bis_size="{&quot;x&quot;:1517,&quot;y&quot;:3629,&quot;w&quot;:28,&quot;h&quot;:19,&quot;abs_x&quot;:1517,&quot;abs_y&quot;:3692}"><i
+                                class="bi-twitter fs-3"
+                                bis_size="{&quot;x&quot;:1517,&quot;y&quot;:3617,&quot;w&quot;:28,&quot;h&quot;:34,&quot;abs_x&quot;:1517,&quot;abs_y&quot;:3680}"></i></a>
+                    </li>
+                    <li class="list-inline-item"
+                        bis_size="{&quot;x&quot;:1572,&quot;y&quot;:3613,&quot;w&quot;:28,&quot;h&quot;:42,&quot;abs_x&quot;:1572,&quot;abs_y&quot;:3676}">
+                        <a href="#!"
+                            bis_size="{&quot;x&quot;:1572,&quot;y&quot;:3629,&quot;w&quot;:28,&quot;h&quot;:19,&quot;abs_x&quot;:1572,&quot;abs_y&quot;:3692}"><i
+                                class="bi-instagram fs-3"
+                                bis_size="{&quot;x&quot;:1572,&quot;y&quot;:3617,&quot;w&quot;:28,&quot;h&quot;:34,&quot;abs_x&quot;:1572,&quot;abs_y&quot;:3680}"></i></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
